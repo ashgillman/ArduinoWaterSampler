fd1ad265e709d859422e4cd813537ae874524443
Ashley Gillman <gillwoman@gmail.com>
2014-07-01 10:30:23 +1000

    V1.0 - Untested as of yet. Should Start and stop pumps, but does not yet wait for float switch.

--- ArduinoWaterSampler.ino
+++ ArduinoWaterSampler.ino
@@ -3,31 +3,39 @@ Arduino Water Sampler
 
 A basic sequencer, waits for an input (from a float sensor indicating presence
 of water), then sequences a series of outputs (activating relays to start the
-sampler pumps).
+sampler PUMP_PINS).
 
-By Ashley Gillman and the Amazing Brendan Calvert
+By Ashley Gillman and Brendan Calvert
 */
 
-//#include "/libraries/Timer/Timer.h" // https://github.com/JChristensen/Timer
+#include <Timer.h> // https://github.com/JChristensen/Timer
+#define DEBUG 1 // Serial out?
 
 // unit converstion constants
-const long hr2milli = 60*60*1000;
-const long min2milli = 60*1000;
-
+const long HR2MILLI = 60*60*1000;
+const long MIN2MILLI = 60*1000;
 // pin constants
-const int pumps[6] = {2, 3, 4, 5, 6, 7};
-
+const int PUMP_PINS[6] = {2, 3, 4, 5, 6, 7};
 // time constants
-const int delays[6] = {0, 1 * hr2milli, 2 * hr2milli, 3 * hr2milli,
-  4 * hr2milli, 5 * hr2milli};
-const int pumpRunTime = 30 * min2milli;
+const int DELAYS[6] = {0, 1 * HR2MILLI, 2 * HR2MILLI, 3 * HR2MILLI,
+  4 * HR2MILLI, 5 * HR2MILLI};
+const int pumpRunTime = 30 * MIN2MILLI;
 
 Timer timer;
 
+int initPumpNo = 0; // Note: zero indexed (0 ~ pump 1)
+int currentPumpNo = 0; // Note: zero indexed (0 ~ pump 1)
+
 void setup() {
-  Serial.begin(9600);
+  if (DEBUG) { Serial.begin(9600); }
   for (int i=0; i<6; i++) {
-    timer.after(delays[i],startPump(i));
+    timer.after(DELAYS[i],startPump);
+    if (DEBUG) {
+      Serial.print("initialised pump ");
+      Serial.print(i + 1);
+      Serial.print(" for ");
+      Serial.println(DELAYS[i]);
+    }
   }
 }
 
@@ -35,11 +43,21 @@ void loop() {
   timer.update();
 }
 
-void startPump(int pumpNo) {
-  digitalWrite(pumps[pumpNo],HIGH);
-  timer.after(pumpRunTime,stopPump(pumpNo));
+void startPump() {
+  digitalWrite(PUMP_PINS[initPumpNo],HIGH); // Pump On
+  timer.after(pumpRunTime,stopPump); // Set timer for off
+  if (DEBUG) {
+    Serial.print("started pump ");
+    Serial.println(initPumpNo + 1);
+  }
+  initPumpNo++;
 }
 
-void stopPump(int pumpNo) {
-  digitalWrite(pumps[pumpNo],LOW);
+void stopPump() {
+  digitalWrite(PUMP_PINS[currentPumpNo],LOW); // Pump Off
+  if (DEBUG) {
+    Serial.print("stopped pump ");
+    Serial.println(currentPumpNo + 1);
+  }
+  currentPumpNo++;
 }
