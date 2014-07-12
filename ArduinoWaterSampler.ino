b7eb16046d8bd21642cefadc7629ad600c7ec1ae
Ashley Gillman <gillwoman@gmail.com>
2014-07-12 13:18:30 +1000

    Display output added

--- ArduinoWaterSampler.ino
+++ ArduinoWaterSampler.ino
@@ -9,6 +9,11 @@ By Ashley Gillman and Brendan Calvert
 */
 
 #include <Timer.h> // https://github.com/JChristensen/Timer
+#include <SPI.h>
+#include <Adafruit_GFX.h> // https://github.com/adafruit/Adafruit-GFX-Library
+#include <Adafruit_PCD8544.h>
+// https://github.com/adafruit/Adafruit-PCD8544-Nokia-5110-LCD-library
+
 #define DEBUG 1 // Serial out?
 
 // unit converstion constants
@@ -29,13 +34,18 @@ const long DELAYS[6] = {
 const long pumpRunTime = 30 * SEC2MILLI;
 
 Timer timer;
+Adafruit_PCD8544 display = Adafruit_PCD8544(7, 6, 5, 4, 3);
 
 int initPumpNo = 0; // Note: zero indexed (0 ~ pump 1)
 int currentPumpNo = 0; // Note: zero indexed (0 ~ pump 1)
+boolean pumpActive[6] = {0, 0, 0, 0, 0, 0};
 
 void setup() {
   if (DEBUG) { Serial.begin(9600); }
+  display.begin();
+  display.setContrast(50);
   for (int i=0; i<6; i++) {
+    pinMode(PUMP_PINS[i],OUTPUT);
     timer.after(DELAYS[i],startPump);
     if (DEBUG) {
       Serial.print("initialised pump ");
@@ -48,23 +58,49 @@ void setup() {
 
 void loop() {
   timer.update();
+  displayTimes();
 }
 
 void startPump() {
   digitalWrite(PUMP_PINS[initPumpNo],HIGH); // Pump On
+  pumpActive[initPumpNo] = true;
   timer.after(pumpRunTime,stopPump); // Set timer for off
   if (DEBUG) {
     Serial.print("started pump ");
-    Serial.println(initPumpNo + 1);
+    Serial.print(initPumpNo + 1);
+    Serial.print(" on pin ");
+    Serial.println(PUMP_PINS[initPumpNo]);
   }
   initPumpNo++;
 }
 
 void stopPump() {
   digitalWrite(PUMP_PINS[currentPumpNo],LOW); // Pump Off
+  pumpActive[currentPumpNo] = false;
   if (DEBUG) {
     Serial.print("stopped pump ");
-    Serial.println(currentPumpNo + 1);
+    Serial.print(currentPumpNo + 1);
+    Serial.print(" on pin ");
+    Serial.println(PUMP_PINS[initPumpNo]);
   }
   currentPumpNo++;
+}
+
+void displayTimes() {
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setCursor(0,0);
+  for (int i=0; i<6; i++) {
+    if (pumpActive[i]) { display.setTextColor(WHITE, BLACK); }
+    else { display.setTextColor(BLACK); }
+    display.print("P");
+    display.print(i + 1);
+    display.setTextColor(BLACK);
+    display.print(": ");
+    display.print(DELAYS[i]/HR2MILLI);
+    display.print("h ");
+    display.print(DELAYS[i]/MIN2MILLI);
+    display.println("min");
+  }
+  display.display();
 }
